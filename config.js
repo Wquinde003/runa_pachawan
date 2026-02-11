27b2ca327da77acb8ff0d7f5183efb6f3f81ca5d
Geovanni Quinde <94090885+Wquinde003@users.noreply.github.com>
2026-02-11 17:42:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    DB_URL: 'mongodb+srv://wquindep:Salesiana@cluster0.xlstmjx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    DB_URL: 'mongodb+srv://usuario:cvlae@cluster0.xlstmjx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     PORT: 3000,
     HOST: 'localhost',
     JWT_SECRET: 'ASDFGHJKLpoiuytrewq123456789zxcvbnm'
